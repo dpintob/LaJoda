c49cb7da5d48dca596505a936a5660789352d8ff
dpintob <dpintob@ucenfotec.ac.cr>
2017-09-26 23:17:15 -0600

    cambio de diseño
    cambio total de diseño, version estable

--- js/effects.js
+++ js/effects.js
@@ -1,1 +1,55 @@
+var myvid = document.getElementById('djvideo');
 
+myvid.addEventListener('ended', function(e) {
+  // get the active source and the next video source.
+  // I set it so if there's no next, it loops to the first one
+  var activesource = document.querySelector("#djvideo source.active");
+  var nextsource = document.querySelector("#djvideo source.active + source") || document.querySelector("#djvideo source:first-child");
+
+  // deactivate current source, and activate next one
+  activesource.className = "";
+  nextsource.className = "active";
+
+  // update the video source and play
+  myvid.src = nextsource.src;
+  myvid.play();
+});
+
+
+// scroll links
+
+$('a[href*="#joda"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
